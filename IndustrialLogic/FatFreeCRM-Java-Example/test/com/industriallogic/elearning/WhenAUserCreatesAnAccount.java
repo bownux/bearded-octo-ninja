fb3fbb6ab09af3693c3821161646b555a69f49cf
Patrick Welsh <patrickwilsonwelsh@gmail.com>
2013-04-02 12:27:04 -0400

    Successfully creating account

--- IndustrialLogic/FatFreeCRM-Java-Example/test/com/industriallogic/elearning/WhenAUserCreatesAnAccount.java
+++ IndustrialLogic/FatFreeCRM-Java-Example/test/com/industriallogic/elearning/WhenAUserCreatesAnAccount.java
@@ -2,43 +2,67 @@ package com.industriallogic.elearning;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
 public class WhenAUserCreatesAnAccount  extends BaseFFCRMTest {
+	private static final String ACCOUNT_NAME = "Bob's Bearings and BassOmatics";
 	private WebElement accountsTab;
 	private WebElement createAccountPanel;
 
 	@Test
 	public void theyCanVerifyThatTheyCreatedIt() throws Exception {
 		accountsTab = goToAccountsTab();
-		addAnAccount("Bob's Bearings and BassOmatics");
+		
+		createAnAccount(ACCOUNT_NAME);
+		verifyAccountCreated(ACCOUNT_NAME);
+		
+		deleteAccount(ACCOUNT_NAME);
+		verifyAccountDeleted(ACCOUNT_NAME);
 	}
 
-	private void addAnAccount(String accountName) {
+	private void createAnAccount(String accountName) throws Exception {
 		createAccountPanel = openAccountCreationPane();
 		fillOutCreateAccountPanel(accountName);
 		
 	}
 
-	private void fillOutCreateAccountPanel(String accountName) {
+	private void fillOutCreateAccountPanel(String accountName) throws Exception {
 		enterNewAccountName(accountName);
 		selectBergbauBranchOption();
 		submitForm();
-		
 	}
 
-	private void submitForm() {
+	private void submitForm() throws Exception {
+		WebElement submit = createAccountPanel.findElement(By.className("buttonbar"));
+		submit.submit();
+		accountsTab = goToAccountsTab();
+		Thread.sleep(15000);
+	}
+	
+	private void verifyAccountCreated(String accountName) {
 		// TODO Auto-generated method stub
 		
 	}
+	
+	private void deleteAccount(String accountName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void verifyAccountDeleted(String accountName) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 
 	private void enterNewAccountName(String accountName) {
 		WebElement accountNameInputField = createAccountPanel.findElement(By.id("account_name"));
 		accountNameInputField.sendKeys(accountName);
 	}
 
-	private void selectBergbauBranchOption() {
+	private void selectBergbauBranchOption() throws Exception {
 		WebElement branchToggle = createAccountPanel.findElement(By.partialLinkText(" Branche"));
 		assertElementVisible(branchToggle);
 		branchToggle.click();
@@ -47,7 +71,10 @@ public class WhenAUserCreatesAnAccount  extends BaseFFCRMTest {
 		branchSelect.click();
 		
 		WebElement bergBauOption = branchSelect.findElement(By.cssSelector("option[value=Bergbau]"));
-		bergBauOption.click();
+		assertElementVisible(bergBauOption);
+		branchSelect.click();
+		branchSelect.sendKeys("b");
+		branchSelect.sendKeys(Keys.ENTER);
 	}
 
 	private WebElement openAccountCreationPane() {
