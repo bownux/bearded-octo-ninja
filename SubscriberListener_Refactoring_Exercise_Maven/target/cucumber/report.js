e22dcff76ccc3f76c0d718574062fb2b7df40e64
Patrick Welsh <patrickwilsonwelsh@gmail.com>
2013-11-13 11:02:56 -0800

    Added another project

--- SubscriberListener_Refactoring_Exercise_Maven/target/cucumber/report.js
+++ SubscriberListener_Refactoring_Exercise_Maven/target/cucumber/report.js
@@ -39,14 +39,14 @@ formatter.match({
   "location": "ConversionStepDefs.Input_Integer_Is(int)"
 });
 formatter.result({
-  "duration": 288590000,
+  "duration": 232197000,
   "status": "passed"
 });
 formatter.match({
   "location": "ConversionStepDefs.We_convert_to_roman_representation()"
 });
 formatter.result({
-  "duration": 178000,
+  "duration": 173000,
   "status": "passed"
 });
 formatter.match({
@@ -59,7 +59,7 @@ formatter.match({
   "location": "ConversionStepDefs.We_get_back_an(String)"
 });
 formatter.result({
-  "duration": 2518000,
+  "duration": 2286000,
   "status": "passed"
 });
 });
